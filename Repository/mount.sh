2f0280fe5ffa588f9573b99b871c0c1d61109de7
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2023-06-13 15:58:16 -0300

    Update mount.sh

--- Repository/mount.sh
+++ Repository/mount.sh
@@ -5,7 +5,7 @@ cryptsetup luksOpen /dev/disk/by-uuid/898221be-388d-4b20-bfdc-74759afb8dce Conta
 mount /dev/mapper/Container-A_crypt /mnt/Local/Container-A
 #
 # SRV02
-sshfs -p 26 emperor@172.16.2.11:/mnt/Local/Container-A/Virt/Images /mnt/Remote/Servers/SRV02/Container-A/Virt/Images/ -o allow_other -o compression=no -o StrictHostKeyChecking=false
+sshfs -p 26 emperor@172.16.10.12:/mnt/Local/Container-A/Virt/Images /mnt/Remote/Servers/SRV02/Container-A/Virt/Images/ -o allow_other -o compression=no -o StrictHostKeyChecking=false
 #
 # Hypervisor
 systemctl restart libvirtd
