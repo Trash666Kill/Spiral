c874e3b854fb448118664522f38e2d962fa95c78
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2023-06-15 09:38:07 -0300

    Update start.sh

--- Repository/start.sh
+++ Repository/start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Mount
+cryptsetup luksOpen /dev/disk/by-uuid/898221be-388d-4b20-bfdc-74759afb8dce Container-A_crypt --key-file /root/.crypt/Container-A.key
+mount /dev/mapper/Container-A_crypt /mnt/Local/Container-A
+sshfs -p 26 emperor@SRV02:/mnt/Local/Container-A/Virt/Images /mnt/Remote/Servers/SRV02/Container-A/Virt/Images/ -o allow_other -o compression=no -o StrictHostKeyChecking=false
 # Interfaces
 # Routes
 # Tunnels
