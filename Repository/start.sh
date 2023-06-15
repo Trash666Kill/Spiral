b3b9be1229c4854b3022bed59e2ccfa1feed04bd
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2023-06-15 09:50:13 -0300

    Update start.sh

--- Repository/start.sh
+++ Repository/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+{(
 set -e
 # Mount
 cryptsetup luksOpen /dev/disk/by-uuid/898221be-388d-4b20-bfdc-74759afb8dce Container-A_crypt --key-file /root/.crypt/Container-A.key
@@ -21,6 +22,7 @@ systemctl restart zabbix-agent
 # Virtual Machines
 #sleep 60
 virsh start VM00
+)}
 # Tunnels
 autossh -M 0 -N -R 2222:localhost:22 -p 4634 emperor@strychnine.duckdns.org -o StrictHostKeyChecking=false
 #
