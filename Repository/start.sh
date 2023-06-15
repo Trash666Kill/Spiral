be47bbf1e3a4708b94fd2179e2df54b6b49227cc
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2023-06-15 09:42:53 -0300

    Update start.sh

--- Repository/start.sh
+++ Repository/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Mount
+{(
 cryptsetup luksOpen /dev/disk/by-uuid/898221be-388d-4b20-bfdc-74759afb8dce Container-A_crypt --key-file /root/.crypt/Container-A.key
 mount /dev/mapper/Container-A_crypt /mnt/Local/Container-A
 sshfs -p 26 emperor@SRV02:/mnt/Local/Container-A/Virt/Images /mnt/Remote/Servers/SRV02/Container-A/Virt/Images/ -o allow_other -o compression=no -o StrictHostKeyChecking=false
@@ -8,6 +9,10 @@ systemctl restart smbd
 systemctl restart zabbix-agent
 #sleep 60
 virsh start VM00
+)}
 # Interfaces
+modprobe dummy
+ip link add zombie0 type dummy
+ip link set zombie0 address 00:00:00:11:11:22
 # Routes
 # Tunnels
