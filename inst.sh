875f9293d53217010f45166db3297074162f23e2
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2023-06-16 10:35:04 -0300

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -76,13 +76,13 @@ cp -v sshd_config /etc/ssh
 rm -v /etc/motd
 cp -v useful /home/emperor/.useful
 touch /etc/motd
-chmod 700 /home/emperor/.ssh
+chmod 600 /home/emperor/.ssh
 su - emperor -c "echo |touch /home/emperor/.ssh/authorized_keys"
 chmod 600 /home/emperor/.ssh/authorized_keys
 #su - emperor -c "echo |ssh-keygen -t rsa -b 4096 -N '' <<<$'\n'" > /dev/null 2>&1
 chmod 600 /root/.isolation
-chmod 700 /root/.crypt
-chmod 700 /root/.ssh
+chmod 600 /root/.crypt
+chmod 600 /root/.ssh
 touch /root/.ssh/authorized_keys
 chmod 600 /root/.ssh/authorized_keys
 #ssh-keygen -t rsa -b 4096 -N '' <<<$'\n' > /dev/null 2>&1
