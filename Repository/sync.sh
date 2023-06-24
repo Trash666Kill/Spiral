70dbf750cbf7c65a0155d75f37be58f24f44a394
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2023-06-23 23:08:40 -0300

    Update sync.sh

--- Repository/sync.sh
+++ Repository/sync.sh
@@ -3,5 +3,6 @@ set -e
 sshfs -p 26 emperor@SRV04.pine:/mnt/Local/A/Music/ /mnt/Remote/Servers/SRV04/Container-A/Music/ -o ro -o allow_other -o compression=no -o StrictHostKeyChecking=false
 #
 su - emperor -c "rsync --bwlimit=20480 -rtu --delete --info=del,name,stats2 --log-file=/var/log/rsync/syncsrv01-`date +%F_%T`.log /mnt/Local/USB/A/Music/ /mnt/Remote/Servers/SRV04/Container-A/Music/"
+find /var/log/rsync/ -name "*.log" -type f -mtime +7 -delete
 #
 /usr/bin/umount /mnt/Remote/Servers/SRV04/Container-A/Music/
