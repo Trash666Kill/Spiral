2023b0b8b9e45e1f03d08628cbed5c4b725ad0dd
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2023-06-29 17:34:43 -0300

    Update sync.sh

--- Repository/sync.sh
+++ Repository/sync.sh
@@ -13,6 +13,6 @@ find /var/log/rsync/ -name "*.log" -type f -mtime +7 -delete
 set -e
 sshfs -p 26 emperor@SRV01.pine:/mnt/Local/Container-B/Backup/SRV02/ /mnt/Remote/Servers/SRV01/Container-B/Backup/SRV02/ -o allow_other -o compression=no -o StrictHostKeyChecking=false
 #rsync -avhW --delete --info=del,name,stats2 --log-file=/var/log/rsync/syncsrv01-`date +%F_%T`.log -e "ssh -p 26" /mnt/Local/Container-A/Virt/Images/VM02.qcow2 emperor@SRV1.pine:/mnt/Local/Container-B/Backup/
-find /var/log/rsync/ -name "*.log" -type f -mtime +7 -delete
+virsh backup-begin AV ./backup.xml
 #
 )}
