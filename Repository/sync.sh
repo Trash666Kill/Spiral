7a8c075f691be6b9652b6630ad5021cf2a3c521b
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2023-07-01 17:37:14 -0300

    Update sync.sh

--- Repository/sync.sh
+++ Repository/sync.sh
@@ -11,7 +11,16 @@ find /var/log/rsync/ -name "*.log" -type f -mtime +7 -delete
 )}
 {(
 set -e
-sshfs -p 26 emperor@SRV01.pine:/mnt/Local/Container-C/Backup/SRV02/ /mnt/Remote/Servers/SRV01/Container-C/Backup/SRV02/ -o allow_other -o compression=no -o StrictHostKeyChecking=false
-virsh backup-begin VM02 ./VM02.xml
+#
+mount SRV01.pine:/mnt/Local/Container-C/Backup/SRV02 /mnt/Remote/Servers/SRV01/Container-C/Backup/SRV02
+cd /mnt/Remote/Servers/SRV01/Container-C/Backup/SRV02/Virt/Images/
+find -name "*qcow2" -type f -mtime +7 -delete 
+(for f in VM08.qcow2
+do
+    mv -n "$f" "$(date -r "$f" +"VM08-%Y%m%d_%H%M%S").qcow2"
+done
+sleep 5)
+cd /etc/scripts/scheduled/
+virsh backup-begin --domain VM08 --backupxml VM08.xml
 #
 )}
