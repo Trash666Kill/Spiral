0425f25da285a440e8ab32fc4974ec9ce48b579c
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2023-07-01 18:41:54 -0300

    Update sync.sh

--- Repository/sync.sh
+++ Repository/sync.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 {(
-set -e
-sshfs -p 26 emperor@SRV04.pine:/mnt/Local/A/Music/ /mnt/Remote/Servers/SRV04/Container-A/Music/ -o ro -o allow_other -o compression=no -o StrictHostKeyChecking=false
-#
-su - emperor -c "rsync --bwlimit=20480 -rtu --delete --info=del,name,stats2 --log-file=/var/log/rsync/syncsrv01-`date +%F_%T`.log /mnt/Local/USB/A/Music/ /mnt/Remote/Servers/SRV04/Container-A/Music/"
-find /var/log/rsync/ -name "*.log" -type f -mtime +7 -delete
-#
-/usr/bin/umount /mnt/Remote/Servers/SRV04/Container-A/Music/
-#
+#set -e
+#sshfs -p 26 emperor@SRV04.pine:/mnt/Local/A/Music/ /mnt/Remote/Servers/SRV04/Container-A/Music/ -o ro -o allow_other -o compression=no -o StrictHostKeyChecking=false
+#su - emperor -c "rsync --bwlimit=20480 -rtu --delete --info=del,name,stats2 --log-file=/var/log/rsync/syncsrv01-`date +%F_%T`.log /mnt/Local/USB/A/Music/ /mnt/Remote/Servers/SRV04/Container-A/Music/"
+#find /var/log/rsync/ -name "*.log" -type f -mtime +7 -delete
+#/usr/bin/umount /mnt/Remote/Servers/SRV04/Container-A/Music/
 )}
-{(
 #
+{(
 mount SRV01.pine:/mnt/Local/Container-C/Backup/SRV02 /mnt/Remote/Servers/SRV01/Container-C/Backup/SRV02
 cd /mnt/Remote/Servers/SRV01/Container-C/Backup/SRV02/Virt/Images/
 find -name "*qcow2" -type f -mtime +7 -delete 
