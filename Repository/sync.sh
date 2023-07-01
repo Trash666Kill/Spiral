9bc2dc7841f6c8205bfda0046f5b224b53aee8f7
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2023-07-01 20:32:37 -0300

    Update sync.sh

--- Repository/sync.sh
+++ Repository/sync.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 {(
-#set -e
-#sshfs -p 26 emperor@SRV04.pine:/mnt/Local/A/Music/ /mnt/Remote/Servers/SRV04/Container-A/Music/ -o ro -o allow_other -o compression=no -o StrictHostKeyChecking=false
-#su - emperor -c "rsync --bwlimit=20480 -rtu --delete --info=del,name,stats2 --log-file=/var/log/rsync/syncsrv01-`date +%F_%T`.log /mnt/Local/USB/A/Music/ /mnt/Remote/Servers/SRV04/Container-A/Music/"
-#find /var/log/rsync/ -name "*.log" -type f -mtime +7 -delete
-#/usr/bin/umount /mnt/Remote/Servers/SRV04/Container-A/Music/
+set -e
+rsync --bwlimit=20480 -rtu --delete --info=del,name,stats2 --log-file=/var/log/rsync/database-`date +%F_%T`.log -e 'ssh -p 26' /mnt/Local/Container-A/Database/ emperor@SRV01.pine:/mnt/Local/Container-C/Backup/SRV02/Database/
+find /var/log/rsync/ -name "*.log" -type f -mtime +7 -delete
 )}
 #
+sleep 5
 {(
 mount SRV01.pine:/mnt/Local/Container-C/Backup/SRV02 /mnt/Remote/Servers/SRV01/Container-C/Backup/SRV02
 cd /mnt/Remote/Servers/SRV01/Container-C/Backup/SRV02/Virt/Images/
@@ -26,7 +25,7 @@ done
 sleep 5
 cd /etc/scripts/scheduled/
 virsh backup-begin --domain VM01 --backupxml VM01.xml
-sleep 600
+sleep 300
 virsh backup-begin --domain VM08 --backupxml VM08.xml
 )}
 #
