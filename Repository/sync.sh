b16b456e0d4059d44a96034e26a81ce728e63ae7
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2023-07-01 17:42:23 -0300

    Update sync.sh

--- Repository/sync.sh
+++ Repository/sync.sh
@@ -17,8 +17,8 @@ find -name "*qcow2" -type f -mtime +7 -delete
 (for f in VM08.qcow2
 do
     mv -n "$f" "$(date -r "$f" +"VM08-%Y%m%d_%H%M%S").qcow2"
-done
-sleep 5)
+done)
+sleep 5
 cd /etc/scripts/scheduled/
 virsh backup-begin --domain VM08 --backupxml VM08.xml
 #
