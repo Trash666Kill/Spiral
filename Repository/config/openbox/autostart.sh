c7fa2533bd47cfb9a26f09504dc1e5f56aa55ae8
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2023-08-14 21:31:14 -0300

    Update autostart.sh

--- Repository/config/openbox/autostart.sh
+++ Repository/config/openbox/autostart.sh
@@ -3,10 +3,8 @@
 #xrandr --output DP-2 --rotate left
 #xrandr --output HDMI-2 --same-as HDMI-1
 # Always on
-#xset -dpms &
-#xset s noblank &
-#xset s off &
-#xset dpms force on &
+xset -dpms &
+xset s off &
 #
 nitrogen --restore &
 lxpanel &
