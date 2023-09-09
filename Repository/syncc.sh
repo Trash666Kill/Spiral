7c3e0b89c770ee8224b6a42c703285601997508a
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2023-09-08 21:58:06 -0300

    Update syncc.sh

--- Repository/syncc.sh
+++ Repository/syncc.sh
@@ -7,33 +7,22 @@ else
 #
 mkdir -v /tmp/vpsbkp/
 cd /tmp/vpsbkp/
-echo "Copying configuration files"
-echo "rc.local"
+
 cp -v /etc/rc.local .
-echo "OpenVPN"
-cp -rv /etc/openvpn .
-echo "Nginx"
-cp -rv /etc/nginx/ .
-echo "Let's Encrypt certificates"
-cp -rv /etc/letsencrypt/ .
+
 echo "Authorized SSH Keys"
 mkdir -pv keys/users/emperor
 mkdir -v keys/users/root
 cp -v /home/emperor/.ssh/authorized_keys keys/users/emperor
 cp -v /root/.ssh/authorized_keys keys/users/root
-echo "Custom scripts"
 cp -rv /etc/scripts .
-echo "Hosts file"
 cp -v /etc/hosts .
-echo "Cron schedule"
+
 cp -rv /var/spool/cron/crontabs/ .
-echo "Emperor user temp"
 cp -rv /home/emperor/Temp .
-echo "Compressing"
 cd ../
 tar -cvzf vpsbkp-`date +%F`.tar.gz vpsbkp/ > /dev/null 2>&1
 chown emperor:emperor vpsbkp-`date +%F`.tar.gz
 rm -r vpsbkp
-echo "Finally, backup is stored in /tmp"
 #
 fi
