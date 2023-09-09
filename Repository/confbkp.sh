1eaaf46a30c2b6e7b702192c438286309f48a1e2
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2023-09-08 22:34:45 -0300

    Update confbkp.sh

--- Repository/confbkp.sh
+++ Repository/confbkp.sh
@@ -17,7 +17,7 @@ cd ../
 chown emperor:emperor -R confbkp
 tar -cvzf confbkp-`date +%F`.tar.gz confbkp/ > /dev/null 2>&1
 chown emperor:emperor confbkp-`date +%F`.tar.gz
-cp -v confbkp-`date +%F`.tar.gz /mnt/Local/Container-C/Backup/SRV01
+su - emperor -c "cp -v confbkp-`date +%F`.tar.gz /mnt/Local/Container-C/Backup/SRV01"
 rm -r confbkp
 rm -v confbkp-`date +%F`.tar.gz
 #
