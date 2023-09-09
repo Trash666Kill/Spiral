705502944791fd5425713b4af5ed056328a85c9a
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2023-09-08 22:02:10 -0300

    Update syncc.sh

--- Repository/syncc.sh
+++ Repository/syncc.sh
@@ -5,23 +5,21 @@ if [[ $EUID -ne 0 ]]; then
         exit 1
 else
 #
-mkdir -v /tmp/vpsbkp/
-cd /tmp/vpsbkp/
-
+mkdir -v /tmp/vpsbkp
+cd /tmp/vpsbkp
+cp -v /etc/network/interfaces .
 cp -v /etc/rc.local .
-
-echo "Authorized SSH Keys"
+cp -rv /etc/scripts .
 mkdir -pv keys/users/emperor
 mkdir -v keys/users/root
 cp -v /home/emperor/.ssh/authorized_keys keys/users/emperor
 cp -v /root/.ssh/authorized_keys keys/users/root
-cp -rv /etc/scripts .
 cp -v /etc/hosts .
-
-cp -rv /var/spool/cron/crontabs/ .
+cp -rv /var/spool/cron/crontabs .
 cp -rv /home/emperor/Temp .
 cd ../
-tar -cvzf vpsbkp-`date +%F`.tar.gz vpsbkp/ > /dev/null 2>&1
+chown emperor:emperor -R vpsbkp
+su - emperor -c "tar -cvzf vpsbkp-`date +%F`.tar.gz vpsbkp/ > /dev/null 2>&1"
 chown emperor:emperor vpsbkp-`date +%F`.tar.gz
 rm -r vpsbkp
 #
