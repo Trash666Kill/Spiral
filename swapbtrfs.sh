9de4334dd98ed3a4d2b4890c97fa6e60c80456fb
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2023-09-19 14:14:59 -0300

    Update swapbtrfs.sh

--- swapbtrfs.sh
+++ swapbtrfs.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   	echo "This script must be run as sudo"
+   	exit 1
+else
+# swapfile
 btrfs subvolume create /swap
 btrfs filesystem mkswapfile --size 8g --uuid clear /swap/swapfile
 swapon /swap/swapfile
