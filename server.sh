9498e4b9a62a6eee416a211be8e624c2a8567db8
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2024-02-10 16:07:22 -0300

    Update server.sh

--- server.sh
+++ server.sh
@@ -94,46 +94,6 @@ touch /root/.ssh/authorized_keys
 chmod 600 /root/.ssh/authorized_keys
 #ssh-keygen -t rsa -b 4096 -N '' <<<$'\n' > /dev/null 2>&1
 /sbin/usermod -aG sudo emperor
-#DE
-echo "**INSTALLING THE DESKTOP ENVIRONMENT**"
-echo "1"
-apt install xorg xserver-xorg-input-libinput xserver-xorg-input-evdev brightnessctl -y
-echo "2"
-apt install xserver-xorg-input-mouse xserver-xorg-input-synaptics -y
-echo "3"
-apt install lightdm openbox obconf lxterminal lxpanel lxhotkey-gtk -y
-echo "4"
-apt install lxtask lxsession-logout lxappearance lxrandr xfce4-power-manager progress -y
-echo "5"
-apt install arc-theme nitrogen x2goserver ffmpegthumbnailer -y
-echo "6"
-apt install gpicview evince galculator gnome-screenshot pluma alacarte -y
-echo "7"
-apt install connman connman-ui connman-gtk compton thunar unrar -y
-echo "8"
-apt install firefox-esr caffeine engrampa gparted gnome-disk-utility baobab -y
-echo "9"
-apt install virt-manager ssh-askpass -y
-#Conf DE
-echo "**SETTING UP THE DESKTOP ENVIRONMENT**"
-rm -v /etc/lightdm/lightdm-gtk-greeter.conf
-cp -v default.jpg /usr/share/wallpapers
-tar -xvf 01-Qogir.tar.xz -C /usr/share/icons > /dev/null 2>&1
-tar -xvf Arc-Dark.tar.xz -C /usr/share/themes > /dev/null 2>&1
-cp -v lightdm-gtk-greeter.conf /etc/lightdm
-cp -v explorer.desktop /usr/share/applications
-cp -v debian-swirl.png /usr/share/icons/default
-mkdir -pv /etc/X11/xorg.conf.d
-# cp -v 40-libinput.conf /etc/X11/xorg.conf.d
-#Emperor
-rm -r /home/emperor/.config
-cp -v gtkrc-2.0 /home/emperor/.gtkrc-2.0
-cp -r config /home/emperor/.config
-chown emperor:emperor -R /home/emperor/.config
-chown emperor:emperor /home/emperor/.gtkrc-2.0
-chown emperor:emperor /usr/share/wallpapers/default.jpg
-#systemctl set-default multi-user.target
-systemctl disable --now x2goserver
 
 #Cleaning up
 echo "**CLEANING UP**"
