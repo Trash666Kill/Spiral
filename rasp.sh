689bb1a159b4a6f54a1521ebb0d83eabba6bb556
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2024-03-20 17:16:40 -0300

    Update rasp.sh

--- rasp.sh
+++ rasp.sh
@@ -154,8 +154,8 @@ printf '#/mnt/Local/Container-A 10.0.0.1(rw,sync,crossmnt,no_subtree_check,no_ro
 {(
 printf '#!/bin/bash
 clamscan --recursive --infected --exclude=Backup --exclude=Virt --exclude=Temp/ISO --log=/var/log/clamav/daily/avscan-`date +%F_%T`.log --move=/root/.isolation /mnt/Local/Container-C
-find /root/.isolation/ -type f -mtime +7 -delete 
-find /var/log/clamav/daily/ -name "*.log" -type f -mtime +2 -delete' > /etc/scripts/scheduled/avscan.sh
+find /root/.isolation -type f -mtime +7 -delete 
+find /var/log/clamav/daily -name "*.log" -type f -mtime +2 -delete' > /etc/scripts/scheduled/avscan.sh
 )}
 chmod +x /etc/scripts/scheduled/avscan.sh
 printf '#!/bin/bash
@@ -181,7 +181,7 @@ cp -v /etc/hosts .
 cp -rv /var/spool/cron/crontabs .
 cp -rv /home/emperor/Temp .
 cd ../
-tar -cvzf confbkp-`date +%F`.tar.gz confbkp/ > /dev/null 2>&1
+tar -cvzf confbkp-`date +%F`.tar.gz confbkp > /dev/null 2>&1
 rm -v /mnt/Local/Pool-A/Backup/SRV01/Container-A/confbkp-`date +%F`.tar.gz
 cp -v confbkp-`date +%F`.tar.gz /mnt/Local/Pool-A/Backup/SRV01/Container-A
 rm -r confbkp
