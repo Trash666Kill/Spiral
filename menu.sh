6d5c2e98a1baa908af0d7a0da6c7c3fee65ad85c
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2024-03-29 13:41:04 -0300

    menu

--- menu.sh
+++ menu.sh
@@ -1,1 +1,52 @@
 #!/bin/bash
+while true; do
+clear
+echo '================================================
+Welcome to the post installation script for Debian minimal. Choose the type of installation you want:
+
+1) Workstation
+
+2) Server
+
+3) Raspberry Pi
+
+4) Exit
+
+================================================'
+
+read -p "Enter the desired installation type and start it by pressing the Enter key: " x
+echo "($x)
+================================================"
+
+case "$x" in
+1)
+free -m
+sleep 5s
+echo "Finished
+================================================"
+exit 0
+;;
+2)
+df -hT
+sleep 5s
+echo "Finished
+================================================"
+exit 0
+;;
+3)
+sensors
+sleep 5s
+echo "Finished
+================================================"
+exit 0
+;;
+4)
+echo "Leaving...
+================================================"
+sleep 5s
+clear
+exit 0
+;;
+*) echo "Invalid option!"
+esac
+done
