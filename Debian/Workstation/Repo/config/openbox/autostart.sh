42b8953fe129bd10aa11c688779d46e9863a2378
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2024-03-30 21:49:03 -0300

    inst

--- Debian/Workstation/Repo/config/openbox/autostart.sh
+++ Debian/Workstation/Repo/config/openbox/autostart.sh
@@ -4,12 +4,10 @@
 #xrandr --output DP-2 --rotate left
 #xrandr --output HDMI-2 --same-as HDMI-1
 # Always on
-#xset -dpms &
-#xset s off &
+xset -dpms &
+xset s off &
 #
 nitrogen --restore &
 lxpanel &
 #virt-manager &
-caffeine-indicator &
 compton &
-xfce4-power-manager &
