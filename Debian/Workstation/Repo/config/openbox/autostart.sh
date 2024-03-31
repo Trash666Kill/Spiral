6009e35faab33dabe8d33b838c0cbebb12a3e9dd
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2024-03-31 14:26:56 -0300

    autostart

--- Debian/Workstation/Repo/config/openbox/autostart.sh
+++ Debian/Workstation/Repo/config/openbox/autostart.sh
@@ -1,9 +1,9 @@
 # Screen settings
-#xrandr -s 1600x900 &
-#xrandr --output HDMI-1 --auto --left-of eDP-1 &
-#xrandr --output eDP-1 --scale 0.75x0.75 &
-#xrandr --output DP-2 --rotate left &
-#xrandr --output HDMI-2 --same-as HDMI-1 &
+#xrandr -s 1600x900
+#xrandr --output HDMI-1 --auto --left-of eDP-1
+#xrandr --output eDP-1 --scale 0.75x0.75
+#xrandr --output DP-2 --rotate left
+#xrandr --output HDMI-2 --same-as HDMI-1
 # Always on
 xset -dpms &
 xset s off &
