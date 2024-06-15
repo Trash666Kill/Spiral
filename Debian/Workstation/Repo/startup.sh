9a7eaa4823f7046d958c37619ef47228f0b352fe
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2024-06-15 20:32:16 -0300

    update

--- Debian/Workstation/Repo/startup.sh
+++ Debian/Workstation/Repo/startup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Mount
-mount -U 8217fdfc-41db-4ed1-af8a-580e69e49bf6 /mnt/Local/Container-A
-cryptsetup luksOpen /dev/disk/by-uuid/6382e4a3-5a30-4c02-bc05-354121e03dd7 USB-A_crypt --key-file /root/.crypt/6382e4a3-5a30-4c02-bc05-354121e03dd7.key
-mount /dev/mapper/USB-A_crypt /mnt/Local/USB/A
-sleep 5
+#mount -U 8217fdfc-41db-4ed1-af8a-580e69e49bf6 /mnt/Local/Container-A
+#cryptsetup luksOpen /dev/disk/by-uuid/6382e4a3-5a30-4c02-bc05-354121e03dd7 USB-A_crypt --key-file /root/.crypt/6382e4a3-5a30-4c02-bc05-354121e03dd7.key
+#mount /dev/mapper/USB-A_crypt /mnt/Local/USB/A
+#sleep 5
 # Swap
-sysctl vm.swappiness=22 #=405,24MiB
-swapon /mnt/Local/Container-A/.swapfile
+#sysctl vm.swappiness=22 #=405,24MiB
+#swapon /mnt/Local/Container-A/.swapfile
 # Interfaces
 #VSW0
 ip tuntap add tap0 mode tap
@@ -28,13 +28,13 @@ iptables -t nat -A POSTROUTING -s 10.0.1.62/26 -o nic0 -j MASQUERADE
 systemctl restart dnsmasq
 systemctl restart lxc
 systemctl restart libvirtd
-systemctl restart smbd
-systemctl restart nfs-kernel-server
+#systemctl restart smbd
+#systemctl restart nfs-kernel-server
 sleep 10
 # Tunnels
-sleep 60
-socat TCP-LISTEN:4533,fork TCP:10.0.0.1:4533 &
-sleep 15
-(
-ssh -f -N -T -R 2222:localhost:26 -p 4634 emperor@strychnine.duckdns.org -o StrictHostKeyChecking=false &
-)
+#sleep 60
+#socat TCP-LISTEN:4533,fork TCP:10.0.0.1:4533 &
+#sleep 15
+#(
+#ssh -f -N -T -R 2222:localhost:26 -p 4634 emperor@strychnine.duckdns.org -o StrictHostKeyChecking=false &
+#)
